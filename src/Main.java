d3a37eb5a4ad25bf3d97c859877e7c6d8829e333
Tomax47 <tammam744@gmail.com>
2023-03-11 20:28:50 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
         File dir = new File("basedir/dir0/");
         int filesNumber=0, subDirectories=0;
 
@@ -37,6 +40,27 @@ public class Main {
         System.out.println("\n");
         printNestedFilesDir(dir,0);
 
+        //f) Print the name of empty directories
+        System.out.println("\nEmpty dir : ");
+        String dirPath = "basedir/";
+        printEmptyDir(dirPath);
+
+        //g) Name of the largest dir
+        System.out.println("\nLargest dir : ");
+        File directory = new File(dirPath);
+        System.out.println("The dir with the most number of files is : "+largestFiledDir(directory));
+
+        //h) Deepest nested path
+        System.out.println("\nLargest dir : ");
+        System.out.println("The deepest nested path is : "+deepestPath(directory));
+
+        //i) Free device space remaining
+        System.out.println("\nRemaining free device space : ");
+        System.out.println("Free remaining space : "+getDeviceFreeSpace(directory)+" MG");
+
+        //j) HardDisks info
+        System.out.println("\nHardDisks info : ");
+        getHDInfo();
     }
 
     public static void getFileName(File dir) {
@@ -92,14 +116,131 @@ public class Main {
         }
     }
 
-    public static void printEmptyDir(File dir) {
-        if (dir.exists() && dir.isDirectory()) {
-            File[] files = dir.listFiles();
-            if (files != null) {
+    public static void printEmptyDir(String path) {
+        File dir = new File(path);
+        if (!dir.isDirectory()) {
+            return;
+        }
 
-            }
+        if (dir.list().length == 0) {
+            System.out.println(dir.getPath());
         } else {
+            for (File file : dir.listFiles()) {
+                printEmptyDir(file.getPath());
+            }
+        }
+    }
+
+    public static void biggestDirName(String path) {
+        File dir = new File(path);
+        File baseDir = new File("basedir/");
+        String biggestDir = dir.getName();
+        if (!dir.isDirectory()) {
+            return;
+        }
+
+        if(dir.list().length != 0 ){
+            for (File file : dir.listFiles()) {
+                if (Integer.parseInt(numberOfFiles(file)) > Integer.parseInt(numberOfFiles(baseDir))) {
+
+                }
+            }
+        }
+    }
+
+    public static int getFilesNum(File dir) {
+        int filesNumber = 0;
+        int subDirectories = 0;
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    filesNumber++;
+                } else if (file.isDirectory()) {
+                    subDirectories++;
+                }
+            }
+        }
+        if (subDirectories > 0) {
+            for (File subDir : files) {
+                if (subDir.isDirectory()) {
+                    filesNumber += getFilesNum(subDir);
+                }
+            }
+        }
+        return filesNumber;
+    }
+
+    public static String largestFiledDir(File dir) {
+        if (!dir.exists() || !dir.isDirectory()) {
             System.out.println("No directory found!");
+            return null;
+        }
+        int maxFileCount = -1;
+        String directoryName = null;
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    int fileCount = getFilesNum(file);
+                    if (fileCount > maxFileCount) {
+                        maxFileCount = fileCount;
+                        directoryName = file.getPath();
+                    }
+                }
+            }
+        }
+        return directoryName;
+    }
+
+
+    public static int getDirDepth(File file) {
+        int depth = 0;
+        while (file.getParentFile() != null) {
+            depth++;
+            file = file.getParentFile();
+        }
+        return depth;
+    }
+
+    public static String deepestPath(File dir) {
+        if (!dir.exists() || !dir.isDirectory()) {
+            System.out.println("No directory found!");
+            return null;
+        }
+        File[] files = dir.listFiles();
+        String deepestPath = dir.getAbsolutePath();
+        int deepestLevel = getDirDepth(dir);
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    String subPath = deepestPath(file);
+                    if (subPath != null) {
+                        int subLevel = getDirDepth(file);
+                        if (subLevel > deepestLevel) {
+                            deepestPath = subPath;
+                            deepestLevel = subLevel;
+                        }
+                    }
+                }
+            }
+        }
+        return deepestPath;
+    }
+
+    public static long getDeviceFreeSpace(File file) {
+        long freeSpace = file.getFreeSpace();
+        return freeSpace/1000000;
+    }
+
+    public static void getHDInfo() {
+        File[] roots = File.listRoots();
+        int numOfDisks = roots.length;
+        System.out.println("Number of storage devices: " + numOfDisks);
+
+        System.out.println("Names of storage devices:");
+        for (int i = 0; i < numOfDisks; i++) {
+            System.out.println(roots[i].getPath());
         }
     }
 }
